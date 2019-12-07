e79f10b47f209fd6ac400c88649e0567240fbbab
luopeng <755200@qq.com>
2019-12-07 14:05:57 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,7 @@
-export GITACCOUNT=
+export GITACCOUNT=tscswcn
 
-export MYCLUSTER=
-export KUBECONFIG=
-
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export MYCLUSTER=tektonknativebeijing16
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing16/kube-config-tok04-tektonknativebeijing16.yml
+export NAMESPACE=tektonknativebeijing16
+export EMAIL=755200@qq.com
+export INGRESS=knative-guoyc.jp-tok.containers.appdomain.cloud
